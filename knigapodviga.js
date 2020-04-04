5a36e5f0bdc441bd26671ad41701bd0d53dd169e
Alexey Kuzovkov <ale-kuzovkov@yandex.ru>
2020-04-04 17:15:41 +0300

    getting human data

---
+++ knigapodviga.js
@@ -0,0 +1,100 @@
+const request 	= require('request')
+const cheerio 	= require('cheerio')
+const Entities = require('html-entities').AllHtmlEntities
+const entities = new Entities()
+const Realm = require('realm')
+const schema = require('./schema')
+const striptags = require('striptags')
+const uuid = require('uuid')
+
+console.log(uuid.v1())
+
+const realm = new Realm({ schema: [schema.Veteran, schema.FacePhoto], schemaVersion: 1, path: 'knigapodviga.realm' })
+
+function timeout (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function main () {
+  const letterURLs = await getLeterURLS()
+  for (const letter of letterURLs) {
+    console.log('Parsing letter', entities.decode(letter.title))
+    const people = await getPeopleURLS(letter.href).catch(e => console.log('Failed getting peoples url', e.message))
+    if (people) {
+      for (const human of people) {
+        const humanName = striptags(entities.decode((human.name)))
+        console.log('Parsing person:', humanName)
+        const humanData = await getHumanData(human.href).catch(e => console.log('Failed to get human data', e.message))
+        if (humanData) {
+          console.log(humanData.replace(/\n\s*\n/g, '\n'))
+          // console.log(humanData.split('\n').reduce((prev, line) => {
+          //   const all = prev || ''
+          //   if (line) return `${all}\n${line}`
+          //   else return all
+          // }, ''))
+          await timeout(10000)
+        }
+      }
+    }
+  }
+}
+
+function getLeterURLS () {
+  return new Promise((resolve, reject) => {
+    request('http://www.knigapodviga.ru/10501085108010751072-1087108610761074108010751072.html', (error, response, html) => {
+      if (error) reject(error)
+      else {
+        const $ = cheerio.load(html)
+        const paragraphs = $('.paragraph')
+        const aElements = $('a', paragraphs[0])
+        const urls = []
+        aElements.each((index, element) => {
+          urls.push({
+            href: $(element).attr('href'),
+            title: $(element).html()
+          })
+        })
+        resolve(urls)
+      }
+    })
+  })
+}
+
+function getPeopleURLS (url) {
+  console.log('get people')
+  return new Promise((resolve, reject) => {
+    request('http://www.knigapodviga.ru/' + url, (error, response, html) => {
+      if (error) reject(error)
+      else {
+        const $ = cheerio.load(html)
+        const paragraphs = $('.paragraph')
+        const aElements = $('a', paragraphs[0])
+        const urls = []
+        aElements.each((index, element) => {
+          urls.push({
+            href: $(element).attr('href'),
+            name: $(element).html()
+          })
+        })
+        resolve(urls)
+      }
+    })
+  })
+}
+
+function getHumanData (url) {
+  return new Promise((resolve, reject) => {
+    console.log('http://www.knigapodviga.ru/' + url)
+    request('http://www.knigapodviga.ru/' + url, (error, response, html) => {
+      if (error) reject(error)
+      else {
+        const $ = cheerio.load(html)
+        const paragraphs = $('.paragraph')
+        const paragraph0Data = striptags(entities.decode($(paragraphs[0]).html()), [], '\n')
+        resolve(paragraph0Data)
+      }
+    })
+  })
+}
+
+main()
