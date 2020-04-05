a780c85e462ea93f0ca38aa75a59b38bb5fa824a
Alexey Kuzovkov <ale-kuzovkov@yandex.ru>
2020-04-05 12:57:14 +0300

    skip

--- README.md
+++ README.md
@@ -8,5 +8,13 @@
 
 ### Запуск парсера ###
 
-* npm install
-* npm start
+`npm install`
+`npm start`
+
+### Параметры ###
+* polk_skip - Пропускает парсинг https://www.moypolk.ru
+* kniga_skip - Пропускает парсинг http://www.knigapodviga.ru
+* filter_skip - Пропускает фильтрацию
+
+Например:
+`npm start polk_skip kniga_skip`
--- index.js
+++ index.js
@@ -3,12 +3,31 @@ const knigapodviga = require('./knigapodviga')
 const filter = require('./filter')
 require('colors')
 
+function timeout (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 async function main () {
-  console.log('Parsing moypolk.ru'.bold.green)
-  await moypolk()
-  console.log('Parsing knigapodviga.ru'.bold.green)
-  await knigapodviga()
-  console.log('Filtering results'.bold.green)
-  filter()
+  if (process.argv.indexOf('polk_skip') < 0) {
+    console.log('Parsing moypolk.ru'.bold.green)
+    await moypolk()
+  } else {
+    console.log('Skipping moypolk.ru'.bold.yellow)
+  }
+  if (process.argv.indexOf('kniga_skip') < 0) {
+    console.log('Parsing knigapodviga.ru'.bold.green)
+    await knigapodviga()
+  } else {
+    console.log('Skipping knigapodviga.ru'.bold.yellow)
+    await timeout(3000)
+  }
+
+  if (process.argv.indexOf('filter_skip') < 0) {
+    console.log('Filtering results'.bold.green)
+    await filter()
+  } else {
+    console.log('Skipping filter'.bold.yellow)
+  }
+  process.exit()
 }
 main()
