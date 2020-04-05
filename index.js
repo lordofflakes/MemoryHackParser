be9d773c953522d3ca4ededb6f6b8eac185d1dba
Alexey Kuzovkov <ale-kuzovkov@yandex.ru>
2020-04-05 12:40:25 +0300

    readme && npm start

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const knigapodviga = require('./knigapodviga')
 const filter = require('./filter')
 
 async function main () {
-  console.log('Parsing knigapodviga.ru')
-  await knigapodviga()
   console.log('Parsing moypolk.ru')
   await moypolk()
+  console.log('Parsing knigapodviga.ru')
+  await knigapodviga()
   console.log('Filtering results')
   filter()
 }
